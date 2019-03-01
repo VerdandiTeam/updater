459d59a4ea552eb30dd59fb1565f935d6274daef
Michal-Szczepaniak <michal_sz13@o2.pl>
2019-03-01 20:46:22 +0100

    Added core to QML

--- src/verdandiupdater-sfos.cpp
+++ src/verdandiupdater-sfos.cpp
@@ -3,11 +3,16 @@
 #endif
 
 #include <sailfishapp.h>
+#include "core.h"
 
 int main(int argc, char *argv[])
 {
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QSharedPointer<QQuickView> view(SailfishApp::createView());
+
+    Core core;
+
+    view->rootContext()->setContextProperty("core", &core);
 
     view->setSource(SailfishApp::pathTo("qml/verdandiupdater-sfos.qml"));
     view->show();
